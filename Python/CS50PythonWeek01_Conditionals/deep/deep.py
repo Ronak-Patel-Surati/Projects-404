49271f9c90d098d2d9688468fd36d6d31900e39e
Ronak Patel <ronitsinghronak@proton.me>
2024-10-20 14:04:10 +0530

    Moved Files

--- Python/CS50PythonWeek01_Conditionals/deep/deep.py
+++
@@ -1,9 +0,0 @@
-#take input
-num = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-num = num.strip().lower()
-#use match
-if num == "forty-two"  or num == "forty two" or num == "42":
-    print("Yes")
-else:
-    print("No")
-
